402ce02586c3a38e11d7e913ef1d2ebac0ce131a
weakow <w@weakow.com>
2020-11-12 21:53:47 +0800

    Restore TS page

---
+++ src/pages/using-typescript.tsx
@@ -0,0 +1,28 @@
+// If you don't want to use TypeScript you can delete this file!
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+type DataProps = {
+  site: {
+    buildTime: string
+  }
+}
+
+const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
+  <Layout>
+    <h1>Gatsby supports TypeScript by default!</h1>
+  </Layout>
+)
+
+export default UsingTypescript
+
+export const query = graphql`
+  {
+    site {
+      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
+    }
+  }
+`
